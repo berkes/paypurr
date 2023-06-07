f6e8f2ae9de8092020fc131cd9680962426f3001
Bèr Kessels <ber@berk.es>
2023-06-07 16:19:09 +0200

    feat: add field "number"

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlElement, HtmlInputElement, HtmlTextAreaElement};
+use web_sys::{HtmlInputElement, HtmlTextAreaElement};
 use yew::prelude::*;
+
+fn initial_number() -> String {
+    "TBD".to_string()
+}
 
 #[function_component]
 fn App() -> Html {
+    let logo_url_handler = use_state(String::default);
+    let logo_url_value = logo_url_handler.to_string();
+
     let to_handler = use_state(String::default);
     let to_value = to_handler.to_string();
+
+    let number_handler = use_state(initial_number);
+    let number_value = number_handler.to_string();
 
     let description_handler = use_state(String::default);
     let description_value =
         Html::from_html_unchecked(format!("<div>{}</div>", *description_handler).into());
-
-    let logo_url_handler = use_state(String::default);
-    let logo_url_value = (*logo_url_handler).clone();
 
     fn create_update_handler<T>(handler: T) -> Callback<Event>
     where
@@ -33,9 +40,10 @@ fn App() -> Html {
         })
     }
 
-    let update_description = create_update_handler(move |value| description_handler.set(value));
     let update_url = create_update_handler(move |value| logo_url_handler.set(value));
     let update_to = create_update_handler(move |value| to_handler.set(value));
+    let update_number = create_update_handler(move |value| number_handler.set(value));
+    let update_description = create_update_handler(move |value| description_handler.set(value));
 
     html! {
         <div class="wrapper">
@@ -49,6 +57,14 @@ fn App() -> Html {
                     onchange={update_url}
                     placeholder="http://example.com/logo-url.png"
                     value={logo_url_value.clone()}
+                />
+
+               <label for="number">{{ "Number" }}</label>
+               <input
+                   type="text"
+                   name="number"
+                   value={number_value.clone()}
+                   onchange={update_number}
                 />
 
                <label for="to">{{ "To" }}</label>
@@ -66,11 +82,13 @@ fn App() -> Html {
                    onchange={update_description}
                 />
             </div>
+
             <div class="preview paper">
                 <img class="logo" src={ logo_url_value } alt="logo" />
                 <br/>
                 <strong>{{"to:"}}</strong><br/>
                 <pre>{{ to_value }}</pre>
+                <p><strong>{{"#"}}</strong>{{ number_value }}</p>
 
                 <p>{{ description_value }}</p>
             </div>
